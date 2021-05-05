7e83c8ce00e1dfb9cac3475a48c1d09824279f4f
Russell Gould [laptop] <gouldru@oregonstate.edu>
2021-05-05 10:30:16 -0700

    add explanatory comments

--- scripts/shell/dada2.sh
+++ scripts/shell/dada2.sh
@@ -4,10 +4,13 @@
 unset R_LIBS
 unset R_LIBS_USER
 
+# this activates the conda environment
 eval "$('/dfs/Megraw_Lab/software_downloads/conda/bin/conda' 'shell.bash' 'hook')"
 conda activate bds_cgrb
 
+# variables taken as input from submission script
 scripts_dir=$1
 seqs_dir=$2
 
+# actually call the R script
 "$scripts_dir"/R/test.R "$seqs_dir"
