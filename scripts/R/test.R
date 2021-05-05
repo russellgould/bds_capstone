072f182c7a6a373236397f5ff2ead939e231a4ca
Russell Gould [laptop] <gouldru@oregonstate.edu>
2021-05-05 08:07:18 -0700

    test more package versions

--- scripts/R/test.R
+++ scripts/R/test.R
@@ -1,9 +1,11 @@
 #!/usr/bin/env Rscript
 
-library(dada2)
-packageVersion("dada2")
+library("dada2"); packageVersion("dada2")
+library("phyloseq"); packageVersion("phyloseq")
+library("ggplot2"); packageVersion("ggplot2")
+library("plyr"); packageVersion("plyr")
 
+# just test that the seqs can be found
 args = commandArgs(trailingOnly = TRUE)
-
 seqs <- args[1]
 list.files(path)
