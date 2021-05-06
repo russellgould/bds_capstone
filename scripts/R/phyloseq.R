ef95f62d1393ef02928791964b5a64ff1e1aebf9
Russell Gould [laptop] <gouldru@oregonstate.edu>
2021-05-05 20:45:25 -0700

    add pauls code

--- scripts/R/phyloseq.R
+++ scripts/R/phyloseq.R
@@ -1,16 +1,36 @@
-library("phyloseq"); packageVersion("phyloseq")
-library("ggplot2"); packageVersion("ggplot2")
-library("plyr"); packageVersion("plyr")
+#!/usr/bin/env Rscript
+
+library("phyloseq")
+library("Biostrings")
+library("ggplot2")
 
 theme_set(theme_bw())
 
-PS <- readRDS("~/large_files/bds/phyloseq/subset_ps")
+args <- commandArgs(trailingOnly = TRUE)
 
-ntaxa(PS)
-nsamples(PS)
+load(args[1])
+silva_nr99_path <- "/nfs3/PHARM/David_Lab/MB599/2020_2021/group1/tax/silva_nr99_v138.1_train_set.fa.gz"
 
-# normalize columns
-PSr <- transform_sample_counts(PS, function(x) x / sum(x))
-# remove columns with mean < 1e-5
-PSfr <- filter_taxa(PSr, function(x) mean(x) > 1e-5, TRUE)
+######################################################
+# Assign taxonomy
+taxa <- assignTaxonomy(seqtab.nochim, silva_nr99_path, multithread = TRUE)
 
+ps <- phyloseq(
+  otu_table(seqtab.nochim, taxa_are_rows = FALSE),
+  tax_table(taxa)
+)
+################# The below two lines were the last two from Maude's DADA2 wizard#################
+sample_sum_df <- data.frame(depth = sample_sums(ps))
+colnames(sample_sum_df) <- c("depth")
+############################### They aren't in the online tutorial###############################
+dna <- Biostrings::DNAStringSet(taxa_names(ps))
+names(dna) <- taxa_names(ps)
+ps <- merge_phyloseq(ps, dna)
+taxa_names(ps) <- paste0("ASV", seq(ntaxa(ps)))
+ps
+# Transform data to proportions as appropriate for Bray-Curtis distances
+ps.prop <- transform_sample_counts(ps, function(otu) otu / sum(otu))
+ord.nmds.bray <- ordinate(ps.prop, method = "NMDS", distance = "bray")
+top20 <- names(sort(taxa_sums(ps), decreasing = TRUE))[1:20]
+ps.top20 <- transform_sample_counts(ps, function(OTU) OTU / sum(OTU))
+ps.top20 <- prune_taxa(top20, ps.top20)
