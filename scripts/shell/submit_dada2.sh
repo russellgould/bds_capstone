a5f70c86647668a6a7b429b72d5622fac7a04828
Russell Gould [laptop] <gouldru@oregonstate.edu>
2021-05-06 07:59:52 -0700

    add comments

--- scripts/shell/submit_dada2.sh
+++ scripts/shell/submit_dada2.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
 
+# make sure this path is correct!
+#   everything is relative to this
+group1_dir=/nfs3/PHARM/David_Lab/MB599/2020_2021/group1
+
+# where is the code located?
+scripts_dir="$group1_dir"/code/scripts
+
+# which input dataset are we using?
+seqs_dir="$group1_dir"/SubsetForTrial
+
+# where do we want the output saved?
+#  plots, Rdata, etc. are saved here
+output_dir="$group1_dir"/subset_output
+
+# these machines have had problems in the past, so we submit to the cluster
+#   and avoid using them
 queue="*@!(symbiosis*|galls*)"
 
-scripts_dir="/raid1/home/bpp/gouldru/bds/scripts"
-seqs_dir="/nfs3/PHARM/David_Lab/MB599/2020_2021/group1/SubsetForTrial"
-
 # submit jobscript
-echo "bash dada2.sh $scripts_dir $seqs_dir" |
+#   -P is # processors
+#   -m is amount of memory
+#   -q is the queue we submit to
+#   -r is where sge saves log files (stderr, stdout)
+echo "bash dada2.sh $scripts_dir $seqs_dir $output_dir" |
     SGE_Array \
-        -P 1 \
+        -P 50 \
         -m 250G \
         -q "$queue" \
         -r sge_logs_dada2
