c18b079f8a5979541fc72d885372b8994210af78
Russell Gould [laptop] <gouldru@oregonstate.edu>
2021-05-18 18:51:41 -0700

    fix arguments

--- scripts/R/create_phyloseq.R
+++ scripts/R/create_phyloseq.R
@@ -9,14 +9,18 @@ theme_set(theme_bw())
 args <- commandArgs(trailingOnly = TRUE)
 
 # NEED INPUT
-# 1) seqtab.nochim: output ASV table from dada2 pipeline
-# 2) silva_nr99_path: taxonomy table
-# 3) sample_data: input data frame with sample data (depth, lat/long)
+# 1) silva_nr99_path: path to taxonomy table
+# 2) r_objs_dir: path to directory for project R objects
+# 3) sample_data: path to file with sample data in it (depth, lat/long)
 
 ######################################################
 # Create phyloseq object
-load(args[1])
-silva_nr99_path <- args[2]
+silva_nr99_path <- args[1]
+
+r_objs_path <- file.path(args[2])
+
+# load the seqtab.nochim object from dada2 output
+load(file.path(r_objs_path, "seqtab_nochim.Rdata"))
 
 if (!file.exists(silva_nr99_path)) {
   stop("Check that the silva_nr99_v138.1_train_set.fa.gz path is correct!")
@@ -41,5 +45,5 @@ ps <- merge_phyloseq(ps, dna)
 taxa_names(ps) <- paste0("ASV", seq(ntaxa(ps)))
 
 # save phyloseq object for posterity
-save(ps, file = file.path(objs_path, "ps.Rdata"))
+save(ps, file = file.path(r_objs_path, "ps.Rdata"))
 ######################################################
