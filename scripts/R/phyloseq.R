057203357b434a5c1adb6c1ea0e0057bb0ddb4fd
Russell Gould [laptop] <gouldru@oregonstate.edu>
2021-05-18 18:35:22 -0700

    create phyloseq object

--- scripts/R/phyloseq.R
+++ scripts/R/phyloseq.R
@@ -8,29 +8,38 @@ theme_set(theme_bw())
 
 args <- commandArgs(trailingOnly = TRUE)
 
-load(args[1])
-silva_nr99_path <- "/nfs3/PHARM/David_Lab/MB599/2020_2021/group1/tax/silva_nr99_v138.1_train_set.fa.gz"
+# NEED INPUT
+# 1) seqtab.nochim: output ASV table from dada2 pipeline
+# 2) silva_nr99_path: taxonomy table
+# 3) sample_data: input data frame with sample data (depth, lat/long)
 
 ######################################################
-# Assign taxonomy
-taxa <- assignTaxonomy(seqtab.nochim, silva_nr99_path, multithread = TRUE)
+# Create phyloseq object
+load(args[1])
+silva_nr99_path <- args[2]
 
+if (!file.exists(silva_nr99_path)) {
+  stop("Check that the silva_nr99_v138.1_train_set.fa.gz path is correct!")
+}
+
+samp_data <- as.data.frame(read.csv(args[3])[, c(2, 4, 5, 6, 7, 8)])
+row.names(samp_data) <- samp_data[, 1]
+samp_data <- samp_data[,2:6]
+
+taxa <-
+  assignTaxonomy(seqtab.nochim, silva_nr99_path, multithread = TRUE)
 ps <- phyloseq(
   otu_table(seqtab.nochim, taxa_are_rows = FALSE),
-  tax_table(taxa)
+  tax_table(taxa),
+  sample_data(samp.data)
 )
-################# The below two lines were the last two from Maude's DADA2 wizard#################
-sample_sum_df <- data.frame(depth = sample_sums(ps))
-colnames(sample_sum_df) <- c("depth")
-############################### They aren't in the online tutorial###############################
-dna <- Biostrings::DNAStringSet(taxa_names(ps))
+
+# rename ASVs to have a short label and save sequences in ps object
+dna <- DNAStringSet(taxa_names(ps))
 names(dna) <- taxa_names(ps)
 ps <- merge_phyloseq(ps, dna)
 taxa_names(ps) <- paste0("ASV", seq(ntaxa(ps)))
-ps
-# Transform data to proportions as appropriate for Bray-Curtis distances
-ps.prop <- transform_sample_counts(ps, function(otu) otu / sum(otu))
-ord.nmds.bray <- ordinate(ps.prop, method = "NMDS", distance = "bray")
-top20 <- names(sort(taxa_sums(ps), decreasing = TRUE))[1:20]
-ps.top20 <- transform_sample_counts(ps, function(OTU) OTU / sum(OTU))
-ps.top20 <- prune_taxa(top20, ps.top20)
+
+# save phyloseq object for posterity
+save(ps, file = file.path(objs_path, "ps.Rdata"))
+######################################################
